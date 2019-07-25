df9ec2866b296a507fe734f284fb4d41869fe7f0
ROME84 <22250311+Rome84@users.noreply.github.com>
2019-07-25 09:19:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-yum install -y wget git
+yum install -y wget
 wget https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
 yum install -y puppet6-release-el-7.noarch.rpm
 yum install -y puppet
-git clone https://github.com/Rome84/nginx-reverse-proxy.git
-cd nginx-reverse-proxy
 puppet module install puppet-nginx
 sed -i 's/localhost/`hostname`/g' nginx.pp
 puppet apply nginx.pp
